66be5af5740f6a2b5e29b0aac7dfebed0110be83
the_daemon_lord <the_daemon_lord69@gmail.com>
2018-11-02 22:37:23 +0530

    Somthings are coming together

--- details/views.py
+++ details/views.py
@@ -3,15 +3,12 @@ from .models import ArtifactDetail
 import os
 from django.conf import settings
 
-file_path = os.path.join(settings.STATIC_ROOT, 'numbers.txt')
+file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'numbers.txt')
+f = open(file_path,'r')
+no_of_rooms = f.read()
+f.close()
 
 
-t1 = ArtifactDetail.objects.filter(room_no__contains="")
-
-def get_query1():
-    k = {i.room_no for i in t1}
-    return k
-count1 = len(get_query1())
 
 room_template = """
 {% load static %}
@@ -20,7 +17,7 @@ room_template = """
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
       <link rel="stylesheet" href="{% static 'css/details.css' %}">
       <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
-        <title>Artifact List</title>
+        <title>Artifact List {{artif}}</title>
     </head>
     <body>
 
@@ -53,18 +50,14 @@ def artifact_list(request):
     artifacts = ArtifactDetail.objects.order_by('room_no')
     return render(request, 'details/artifact_list.html', {'artifacts':artifacts})
 
-t2 = ArtifactDetail.objects.filter(room_no__contains="")
 
-def get_query2():
-    k = {i.room_no for i in t2}
-    return k
-
-count2 = len(get_query2())
 
 def make_html():
-    if count2 - count1 !=0:
-        for i in range(count2-count1):
-            with open('templates/details/'+"room_number"+str(count1+i+1)+".html",'w+') as f:
-                f.write(room_template)
+    for i in range(int(no_of_rooms)+1):
+        if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html")):
+            f=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html"),'r+')
+        else:
+            fila=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html"),'w+')
+            fila.write(room_template)
 
 make_html()
