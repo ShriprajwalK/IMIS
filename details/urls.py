8453121c24758696427860c3f99d86ca056ec7ff
the_daemon_lord <the_daemon_lord69@gmail.com>
2018-11-03 00:00:05 +0530

    Turned into crap

--- details/urls.py
+++ details/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .models import ArtifactDetail
+
+a = ArtifactDetail.objects.filter(room_no__contains="")
+k = a[len(a)-1].room_no
 
 urlpatterns = [
     path('', views.artifact_list, name='artifact_list'),
-]
+    path('room1',)
+    ]
--- details/views.py
+++ details/views.py
@@ -3,13 +3,14 @@ from .models import ArtifactDetail
 import os
 from django.conf import settings
 
+#if a new artifact is added to an already present room... we haven't considered that.....yet
+
 file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'numbers.txt')
 f = open(file_path,'r')
 no_of_rooms = f.read()
 f.close()
 
-
-
+'''
 room_template = """
 {% load static %}
 <html>
@@ -43,21 +44,32 @@ room_template = """
 </body>
 </html>
 """
-
+'''
 
 
 def artifact_list(request):
-    artifacts = ArtifactDetail.objects.order_by('room_no')
+    artifacts = ArtifactDetail.objects.filter(room_no__contains="").order_by('room_no')
     return render(request, 'details/artifact_list.html', {'artifacts':artifacts})
 
 
+def artifacts_in_room(room_no):
+    artifacts = ArtifactDetail.objects.filter(room_no__contains=room_no)
+    return artifacts
+
+artifacts_in_room(1)
+
+def room_no(i,request):
+    artifacts = artifacts_in_room()
+    return render(request, 'details/room_number1.html', {'artifacts' : artifacts})
 
 def make_html():
     for i in range(int(no_of_rooms)+1):
-        if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html")):
-            f=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html"),'r+')
-        else:
-            fila=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html"),'w+')
-            fila.write(room_template)
+        fila=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/details/'+"room_number"+str(i+1)+".html"),'w+')
+        artifacts_in_room = ArtifactDetail.objects.filter(room_no__contains = str(i))
+
+        #fila.write(room_template)
+        fila.close()
+
+
 
 make_html()
