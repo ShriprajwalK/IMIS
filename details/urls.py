d4c64d34cc471de94fe5b51a0ae3f32004dae5e3
kimi no nawa <you@example.com>
2018-11-03 15:48:13 +0530

    IT WORKS AND IS BOOTIFUL2

--- details/urls.py
+++ details/urls.py
@@ -8,7 +8,6 @@ from . import views
 urlpatterns = [
     path('', views.artifact_list, name='artifact_list'),
     path('room/<int:i>',views.room, name = "lola"),
-    path('search',views.search,name = 'search'),
     path('feedback', views.feedback, name = "feedback"),
     path('success.html',views.success, name = 'success'),
     path('aravind',views.aravind,name = 'aravind')
